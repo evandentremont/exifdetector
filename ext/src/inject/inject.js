980cc74d0acef39782da579dcffdf828b238c38e
Evan d'Entremont <evan@goalline.ca>
2015-03-24 15:00:28 -0300

    tidied up formatting

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,26 +1,20 @@
+function dms2dec(d, m, s){
+	dec = Math.abs(d) + (m / 60.0) + (s / 3600.0);
+	dec = (d > 0) ? dec : (0-d);
+	return dec;
+}
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
-		//http://ptforum.photoolsweb.com/ubbthreads.php?ubb=download&Number=1024&filename=1024-2006_1011_093752.jpg
-
+		
 		var s = document.createElement('script');
-
-		// TODO: add "script.js" to web_accessible_resources in manifest.json
 		s.src = chrome.extension.getURL('src/inject/exif.js');
 		s.onload = function() {
 			this.parentNode.removeChild(this);
 		};
 		(document.head||document.documentElement).appendChild(s);
-
-
-		function dms2dec(d, m, s){
-			dec = Math.abs(d) + (m / 60.0) + (s / 3600.0);
-			dec = (d > 0) ? dec : (0-d);
-			return dec;
-		}
-
 
 		var images = document.getElementsByTagName('img');
 		for(var i=0; i < images.length; i++) {
@@ -39,19 +33,9 @@ chrome.extension.sendMessage({}, function(response) {
 				else {
 					console.log(this.src + ' has no GPS data')
 					this.style.border = "1px solid red";
-
-
 				}
-
-
-
 			});
 		}
-
-
-
-
-
 	}
 	}, 10);
 });
